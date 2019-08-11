38544ddd25fb86f3daed5dee9d9d26427d0a080f
ck182666 <ck182666@hotmail.com>
2019-08-12 07:34:40 +0800

    1st commit

---
+++ gittest.php
@@ -0,0 +1,14 @@
+<html>
+<header><title>test</title></header>
+<body>
+<p>*********************乘法表***************<br>
+
+<form name="form1" action="test3_1.php" method="get"> 
+請輸入數字 
+<input type="text" name="m1" size=10 maxlength=10>*
+<input type="text" name="m2" size=10 maxlentth=10>
+<INPUT type="submit" value="送出">
+<INPUT type="reset" value="重新輸入"><br>
+<p>
+<body>
+</html>
