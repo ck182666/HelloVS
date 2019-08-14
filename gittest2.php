1286f0be67c499d5cb7aeed08ed2d00cc49bc1d4
ck182666 <ck182666@hotmail.com>
2019-08-14 22:09:00 +0800

    commit on 20190814

--- gittest2.php
+++ gittest2.php
@@ -1,5 +1,5 @@
 <html>
-<header><title>test</title></header>
+<header><title>test2</title></header>
 <body>
 <p>*********************乘法表***************<br>
 
@@ -9,6 +9,6 @@
 <input type="text" name="m2" size=10 maxlentth=10>
 <INPUT type="submit" value="送出">
 <INPUT type="reset" value="重新輸入"><br>
-<p>**********1st modified*****
+<p>
 <body>
 </html>
